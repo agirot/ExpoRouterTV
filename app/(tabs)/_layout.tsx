461a48a721821fbbb708c1ac573dfd0808410d14
Adrien GIROT <agirot@online.net>
2025-02-04 20:43:39 +0100

    fix(build): bump deps, fix tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { withLayoutContext } from 'expo-router';
-import { createNativeBottomTabNavigator } from 'react-native-bottom-tabs/react-navigation';
+import {
+    createNativeBottomTabNavigator,
+} from '@bottom-tabs/react-navigation';
+
 import { Platform } from 'react-native';
 
 export const Tabs = withLayoutContext(
@@ -16,7 +19,6 @@ export default function TabLayout() {
     <Tabs
       tabBarActiveTintColor={colors.tabIconSelected}
       tabBarInactiveTintColor={colors.tabIconDefault}
-      barTintColor={colors.background}
       rippleColor={colors.tint}
       labeled={true}
     >
